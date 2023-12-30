2a11917d7bc1b419b60fdb1f3b8cfe427b57ddc4
minthantkyaw28 <wanderhobby@gmail.com>
2023-12-30 20:20:12 +0630

    c31

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-	"./node_modules/tw-elements/dist/js/**/*.js"],
+	"./node_modules/tw-elements/dist/js/**/*.js",
+	'./node_modules/flowbite/**/*.js'],
+	
 	theme: {
 		extend: {},
 	},
-	plugins: [require("tw-elements/dist/plugin.cjs")],
+	plugins: [require("tw-elements/dist/plugin.cjs"),require('flowbite/plugin')],
 }
